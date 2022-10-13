7f939861a1c5b1a59ab6c5c5df0d2069a7c3b353
Najmi Anasya Calyla <najmi.anasya@ui.ac.id>
2022-10-13 08:50:40 +0700

    Edit delete function

--- todolist/urls.py
+++ todolist/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('create-task/', create_task, name='create-task'),
     path('update-task/<int:id>', update_task, name='update-task'),
-    path('delete-task/<int:id>', delete_task_ajax, name='delete_task_ajax'),
+    path('delete-task/<int:id>', delete_task, name='delete-task'),
     path('json/', show_todolist_json, name='show_todolist_json'),
-    path('add/', add_task, name='add_task')
+    path('add/', add_task, name='add_task'),
+    path('delete/<int:id>', delete_task_ajax, name='delete_task_ajax'),
 ]
